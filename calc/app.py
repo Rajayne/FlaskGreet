70148336778870fa9e69b19429656a9cb5f6787e
Rajayne <ryann.j.nealey@gmail.com>
2023-02-09 10:24:58 -0800

    Instead of series of if statements, created functions object and get command to determine which math function to use

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,22 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/math/<function>')
+def calc(function):
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = FUNCTIONS.get(function, "Not a valid function")
+    if result == "Not a valid function":
+        return result
+    else:
+        return f"{a} {function} by {b} equals {result(a,b)}"
+
+FUNCTIONS = {
+    'add': add,
+    'subtract': sub,
+    'multiply': mult,
+    'divide': div
+}
